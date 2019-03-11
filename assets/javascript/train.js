c883268ccae0499da750049f8499cdb5f059d923
Weslley Braga <wesbragagt@gmail.com>
2019-03-11 17:02:05 -0500

    final commit with it's full fuctionality and design

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,10 +1,8 @@
-// CLOCK, time today 
+// CLOCK, time today
 setInterval(function() {
-    var today = moment().format("hh:mm:ss a");
-    $("#current-time").text((today));
+  var today = moment().format("hh:mm:ss a");
+  $("#current-time").text(today);
 }, 1000);
-
-
 
 // FIREBASE SETUP
 var config = {
@@ -39,8 +37,6 @@ $("#train-submit").on("click", function(event) {
   newTrain.time = $("#first-train-input").val();
   newTrain.frequency = $("#frequency").val();
 
-  
-
   database.ref().push({
     trainName: newTrain.name,
     destination: newTrain.destination,
@@ -53,66 +49,36 @@ $("#train-submit").on("click", function(event) {
 // Now populate the page with data from firebase
 
 database.ref().on(
-    "child_added",
-    function(childSnapshot) {
-      // console.log(childSnapshot.val().trainName);
-      // console.log(childSnapshot.val().destination);
-      // console.log(childSnapshot.val().time);
-      // console.log(childSnapshot.val().frequency);
-      
-      var snapVal = childSnapshot.val();
-      
-      var firstArrive = moment(snapVal.time, "hh:mm").format("hh:mm");
-      var now = moment().format("hh:mm");
-      var minAway;
-  
-      
-      
-      
-      // every time the train has not yet passed
-      while (moment().unix() > moment(snapVal.time, "hh:mm").unix()){
-        //   alert("works");
-        snapVal.time = moment(snapVal.time, "hh:mm").add(snapVal.frequency, "m");
-        minAway = moment(snapVal.time, "hh:mm").diff(moment(), "m");
-         }
+  "child_added",
+  function(childSnapshot) {
+    var snapVal = childSnapshot.val();
+    var minAway;
 
-    // console.log(moment(snapVal.time, "hh:mm").add(snapVal.frequency, "m").format("hh:mm a"));
-        
-    // while(moment(snapVal.time, "hh:mm").unix() <= moment().unix()){
-    //     snapVal.time = ;
-        
-    // }
-
-    //   if(moment(now, "hh:mm").unix() >= moment(snapVal.time, "hh:mm").unix()){
-    //     //   alert("works")
-    //       snapVal = moment(snapVal.time, "hh:mm").add(snapVal.frequency, "m").format("hh:mm a");
-    //   }
-  
-      // every time the train has passed
-    //   if (moment(now,"hh:mm").unix() >= moment(firstArrive, "hh:mm").unix()){
-    //       snapVal.time = moment(snapVal.time, "hh:mm").add(snapVal.frequency, "m");
-          
-    //   }
-  
-      
-      var trainDom = {
-        name: snapVal.trainName,
-        destination: snapVal.destination,
-        time: moment(snapVal.time, "hh:mm").format("hh:mm a"),
-        frequency: snapVal.frequency,
-        minAway: minAway
-      };
-  
-      var newTr = $("<tr>");
-  
-      // loop through the object and populate the dom with table rows
-      $.each(trainDom, function(key, value) {
-        $(newTr).append("<td>" + value + "</td>");
-      });
-  
-      $(".train-data").append(newTr);
-    },
-    function(errorObject) {
-      alert(errorObject.data);
+    // every time the train has not yet passed
+    while (moment().unix() >= moment(snapVal.time, "hh:mm").unix()) {
+      //   alert("works");
+      snapVal.time = moment(snapVal.time, "hh:mm").add(snapVal.frequency, "m");
+      minAway = moment(snapVal.time, "hh:mm").diff(moment(), "m");
     }
-  );
+
+    var trainDom = {
+      name: snapVal.trainName,
+      destination: snapVal.destination,
+      time: moment(snapVal.time, "hh:mm").format("hh:mm a"),
+      frequency: snapVal.frequency,
+      minAway: minAway
+    };
+
+    var newTr = $("<tr>");
+
+    // loop through the object and populate the dom with table rows
+    $.each(trainDom, function(key, value) {
+      $(newTr).append("<td>" + value + "</td>");
+    });
+
+    $(".train-data").append(newTr);
+  },
+  function(errorObject) {
+    alert(errorObject.data);
+  }
+);
