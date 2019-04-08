a1ccf1b07051b22fb60c9e73a9267883db7f0e4f
Weslley Braga <wesbragagt@gmail.com>
2019-04-08 01:35:24 -0500

    trying to implement real time updates with minutes away

--- assets/javascript/bus.js
+++ assets/javascript/bus.js
@@ -2,6 +2,46 @@
 setInterval(function() {
     var today = moment().format("hh:mm:ss a");
     $("#current-time").text(today);
+    database.ref().on(
+        "child_added",
+        function(childSnapshot) {
+            var snapVal = childSnapshot.val();
+            var minAway;
+
+            // every time the train has not yet passed
+            while (moment().unix() >= moment(snapVal.time, "hh:mm").unix()) {
+                //   alert("works");
+                snapVal.time = moment(snapVal.time, "hh:mm").add(
+                    snapVal.frequency,
+                    "m"
+                );
+                minAway = moment(snapVal.time, "hh:mm").diff(moment(), "m");
+            }
+            if (minAway === undefined) {
+                minAway = moment(snapVal.time, "hh:mm").diff(moment(), "s");
+            }
+
+            var trainDom = {
+                name: snapVal.trainName,
+                destination: snapVal.destination,
+                time: moment(snapVal.time, "hh:mm").format("hh:mm a"),
+                frequency: snapVal.frequency,
+                minAway: minAway
+            };
+
+            var newTr = $("<tr>");
+
+            // loop through the object and populate the dom with table rows
+            $.each(trainDom, function(key, value) {
+                $(newTr).append("<td>" + value + "</td>");
+            });
+
+            $(".train-data").html(newTr);
+        },
+        function(errorObject) {
+            alert(errorObject.data);
+        }
+    );
 }, 1000);
 
 // FIREBASE SETUP
@@ -47,44 +87,3 @@ $("#train-submit").on("click", function(event) {
 });
 
 // Now populate the page with data from firebase
-
-database.ref().on(
-    "child_added",
-    function(childSnapshot) {
-        var snapVal = childSnapshot.val();
-        var minAway;
-
-        // every time the train has not yet passed
-        while (moment().unix() >= moment(snapVal.time, "hh:mm").unix()) {
-            //   alert("works");
-            snapVal.time = moment(snapVal.time, "hh:mm").add(
-                snapVal.frequency,
-                "m"
-            );
-            minAway = moment(snapVal.time, "hh:mm").diff(moment(), "m");
-        }
-        if (minAway === undefined) {
-            minAway = moment(snapVal.time, "hh:mm").diff(moment(), "m");
-        }
-
-        var trainDom = {
-            name: snapVal.trainName,
-            destination: snapVal.destination,
-            time: moment(snapVal.time, "hh:mm").format("hh:mm a"),
-            frequency: snapVal.frequency,
-            minAway: minAway
-        };
-
-        var newTr = $("<tr>");
-
-        // loop through the object and populate the dom with table rows
-        $.each(trainDom, function(key, value) {
-            $(newTr).append("<td>" + value + "</td>");
-        });
-
-        $(".train-data").append(newTr);
-    },
-    function(errorObject) {
-        alert(errorObject.data);
-    }
-);
